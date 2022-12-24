56fdf029c6bd91660bbac090f207dfbf7eca298d
Joel Armendariz <jarmendariz@evernote.com>
2022-12-24 14:58:49 -0600

    clean up generate_test_account script

--- scripts/evernote/generate_test_account/main.py
+++ scripts/evernote/generate_test_account/main.py
@@ -2,23 +2,22 @@ import subprocess
 import os
 import sys
 
-
-def generate_email_with_account_number(account_number):
-    return "jarmendariz+ion" + account_number + "@evernote.com"
-
-
+dir = os.path.dirname(os.path.realpath(__file__))
 account_number_override = sys.argv[-1] if len(sys.argv) > 1 else None
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
 
-with open(dir_path + "/current_account_number.txt", "r") as f:
+def generate_email_with_account_number(account_number):
+    return f"jarmendariz+ion{account_number}@evernote.com"
+
+
+with open(f"{dir}/current_account_number.txt", "r") as f:
     current_account_number = f.read()
 
 new_account_number = account_number_override or str(int(current_account_number) + 1)
 generated_email = generate_email_with_account_number(new_account_number)
-subprocess.check_call("echo " + generated_email + " | pbcopy", shell=True)
+subprocess.check_call(f"echo {generated_email} | pbcopy", shell=True)
 
-with open(dir_path + "/current_account_number.txt", "w") as f:
+with open(f"{dir}/current_account_number.txt", "w") as f:
     f.write(new_account_number)
 
-print("Copied " + generated_email + " to clipboard")
+print(f"Copied {generated_email} to clipboard")
