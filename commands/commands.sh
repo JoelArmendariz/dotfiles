c5b541cf0581b9df1b3f93959e99fc487c064845
Joel Armendariz <armendariz.joel23@gmail.com>
2022-12-26 09:31:12 -0600

    add requiem commands

--- commands/commands.sh
+++ commands/commands.sh
@@ -1,15 +1,21 @@
 #!bin/zsh
 
+# General/Personal
 source ~/.config/commands/git.sh
 source ~/.config/commands/qmk.sh
 source ~/.config/commands/quick_files.sh
 source ~/.config/commands/quick_directories.sh
 source ~/.config/commands/tmux.sh
 source ~/.config/commands/utils.sh
+
+# Evernote
 source ~/.config/commands/evernote/tmux.sh
 source ~/.config/commands/evernote/quick_directories.sh
 source ~/.config/commands/evernote/quick_files.sh
 source ~/.config/commands/evernote/commands.sh
+
+# Requiem
+source ~/.config/commands/requiem/project_scripts.sh
 
 function source_commands() {
     source ~/.config/commands/commands.sh
---
+++ commands/requiem/project_scripts.sh
@@ -0,0 +1,27 @@
+#!bin/zsh
+
+# Start the Data Requiem local API
+function start_api() {
+    cd ~/Documents/Development/requiem-mono-repo/requiem/services/requiem-api/
+    poetry run uvicorn --app-dir src --reload --port 5000 app.main:app
+}
+
+# Start the Data Requiem local UI
+function start_ui() {
+    cd ~/Documents/Development/requiem-mono-repo/requiem/services/requiem-ui/
+    yarn dev
+}
+
+function generate_requiem_models() {
+    cd ~/Documents/Development/requiem-mono-repo/requiem/libraries/models-py/scripts/
+    poetry run sh scripts/generate.sh
+}
+
+function secretsyml() {
+    nvim /etc/requiem/secrets.yml
+}
+
+function settingsyml() {
+    nvim /etc/requiem/settings.yml
+}
+
