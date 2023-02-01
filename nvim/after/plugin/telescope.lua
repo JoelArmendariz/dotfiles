c27ed12b42423ba5d11d8d5236233688f3019351
Joel Armendariz <armendariz.joel23@gmail.com>
2023-02-01 07:59:04 -0600

    change find_files to leader f

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -2,8 +2,7 @@ local builtin = require('telescope.builtin')
 local telescope = require('telescope')
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>f', builtin.current_buffer_fuzzy_find, {})
+vim.keymap.set('n', '<leader>f', builtin.git_files, {})
 vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Find All Query > ") });
