60841209764c137293c60f70fe51aa0fc98ab5b1
Joel Armendariz <joelarmendariz@Joels-MBP.attlocal.net>
2023-02-27 12:17:26 -0600

    add some requiem scripts

--- commands/requiem/project_scripts.sh
+++ commands/requiem/project_scripts.sh
@@ -39,3 +39,60 @@ function settingsyml() {
     nvim /etc/requiem/settings.yml
 }
 
+export RQ_EC2_NAME="jarmendariz-dev" # replace with your instance name
+export RQ_EC2_PEM="~/.ssh/jarmendariz-macbookpro.pem" # replace with your .pem name
+
+function rq::ec2::start() {
+instance_name=${1:-$RQ_EC2_NAME}
+echo "Starting EC2 instance $instance_name"
+    aws ec2 describe-instances \
+        --filters "Name=tag:Name,Values=$instance_name" \
+ --query "Reservations[0].Instances[0].InstanceId" | \
+ xargs -I {} aws ec2 start-instances \
+ --instance-ids {}
+}
+
+function rq::ec2::stop() {
+instance_name=${1:-$RQ_EC2_NAME}
+echo "Stopping EC2 instance $instance_name"
+    aws ec2 describe-instances \
+        --filters "Name=tag:Name,Values=$instance_name" \
+ --query "Reservations[0].Instances[0].InstanceId" | \
+ xargs -I {} aws ec2 stop-instances \
+ --instance-ids {}
+}
+
+function rq::ec2::status() {
+instance_name=${1:-$RQ_EC2_NAME}
+echo "Checking status of EC2 instance $instance_name"
+    aws ec2 describe-instances \
+        --filters "Name=tag:Name,Values=$instance_name" \
+ --query "Reservations[0].Instances[0].State.Name" \
+ --output text
+}
+
+function rq::ec2::info() {
+instance_name=${1:-$RQ_EC2_NAME}
+echo "Checking details of EC2 instance $instance_name"
+    aws ec2 describe-instances \
+        --filters "Name=tag:Name,Values=$instance_name"
+}
+
+function rq::ec2::ssh() {
+instance_name=${1:-$RQ_EC2_NAME}
+echo "Connecting to EC2 instance $instance_name"
+    ip=`aws ec2 describe-instances --filters "Name=tag:Name,Values=$instance_name" --query "Reservations[0].Instances[0].PublicIpAddress" --output text`
+echo $ip
+    ssh -i ~/.ssh/jarmendariz-macbookpro.pem ubuntu@$ip
+}
+
+function rq::ec2::docker_context() {
+instance_name=${1:-$RQ_EC2_NAME}
+echo "Setting up docker context to EC2 instance $instance_name"
+    ip=`aws ec2 describe-instances --filters "Name=tag:Name,Values=$instance_name" --query "Reservations[0].Instances[0].PublicIpAddress" --output text`
+docker context use default
+docker context remove $instance_name || true
+    docker context create $instance_name --docker host=ssh://ubuntu@$ip
+docker context use $instance_name
+}
+
